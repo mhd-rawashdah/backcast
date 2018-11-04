2f2bd0b232a8dad3cc42e20c464d15349a318a6a
gmajali <ghazi_majali@outlook.com>
2018-11-05 00:26:18 +0200

    refactor based on video solution

--- src/views/app.js
+++ src/views/app.js
@@ -3,12 +3,32 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+  	// create object from collection and pass array of model
+    this.videos = new Videos(exampleVideoData);
+    // call render method
+    this.render();
   },
 
 
   render: function() {
     this.$el.html(this.template());
+    // connection listView with  appView parent by cearte object and pass el (the class name of elemet 
+    //in html in JQuery) and call render method  
+    new VideoListView({
+    	el: this.$('.list'),
+    	collection: this.videos}
+    	).render();
+    // connection searchView with  appView parent by cearte object and pass el (the class name of elemet 
+    //in html in JQuery ) and call render method  
+    new SearchView({
+    	el: this.$('.search')}
+    	).render();
+    // connection VideoPlayer with  appView parent by cearte object and pass el (the class name of elemet in
+    // html in JQuery )and pass model and call render method  
+    new VideoPlayerView({
+    	el: this.$('.player'), 
+    	model: this.videos.at(0)}
+    	).render();
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,10 +1,18 @@
 var VideoListView = Backbone.View.extend({
-
-
+ 
+ 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+    this.collection.each(this.renderVideo, this);
     return this;
+  },
+
+  renderVideo: function(video){
+  	console.log(video);
+  	this.$('.video-list').append (
+  		new VideoListEntryView({model: video}).render().el);
+		
   },
 
   template: templateURL('src/templates/videoList.html')
